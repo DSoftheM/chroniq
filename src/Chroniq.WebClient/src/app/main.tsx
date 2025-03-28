86d526c259d0a133e906dda44b86cf6cad012cbf
artur <email@example.com>
2025-03-28 12:25:10 +0500

    Удалены устаревшие файлы приложения, включая компоненты маршрутизации, глобальные стили и основной файл приложения. Это упрощает структуру проекта и устраняет неиспользуемый код.

--- src/Chroniq.WebClient/src/app/main.tsx
+++ src/Chroniq.WebClient/src/app/main.tsx
@@ -2,10 +2,10 @@ import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 import App from "./App.tsx"
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
-import { GlobalStyles } from "./global-styles.ts"
+import { GlobalStyles } from "./styles/global-styles.ts"
 import { ThemeProvider } from "styled-components"
 import { ConfigProvider, theme } from "antd"
-import { NotificationProvider } from "./global/notification-provider.tsx"
+import { NotificationProvider } from "./providers/notification-provider.tsx"
 
 const client = new QueryClient({
   defaultOptions: {
--- src/Chroniq.WebClient/src/app/router.tsx
+++ src/Chroniq.WebClient/src/app/router.tsx
@@ -1,12 +1,12 @@
 import { createHashRouter } from "react-router-dom"
-import { MainLayout } from "./features/main/main-layout"
-import { SettingsPage } from "./features/settings/settings-page"
-import { LoginPage } from "./features/auth/login-page"
-import { nav } from "./lib/nav"
-import { Page404 } from "./features/_404/page404"
-import { SchedulePage } from "./features/schedule/schedule-page"
-import { ProfilePage } from "./features/profile/profile-page"
-import { RegisterPage } from "./features/auth/register-page"
+import { MainLayout } from "../features/main/ui/main-layout"
+import { SettingsPage } from "../features/settings/ui/settings-page"
+import { LoginPage } from "../features/auth/ui/login-page"
+import { nav } from "../shared/lib/nav"
+import { Page404 } from "../features/_404/ui/page404"
+import { SchedulePage } from "../features/schedule/ui/schedule-page"
+import { ProfilePage } from "../features/profile/ui/profile-page"
+import { RegisterPage } from "../features/auth/ui/register-page"
 
 export const router = createHashRouter([
   {
